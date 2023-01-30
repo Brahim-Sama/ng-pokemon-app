8f64e0910786b5c059d57abeddbe9cf534e8b55f
brahim ouass <brahim.ouass@hotmail.fr>
2023-01-30 15:09:33 +0100

    Pokemon Service

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PokemonService {
+  //récupère la liste de pokemon
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
 
-  constructor() { }
+  // récupère le pokemon selon son ID
+  getPokemonById(pokemonId: number): Pokemon | undefined {
+    return POKEMONS.find((pokemon) => pokemon.id == pokemonId);
+  }
+
+  //récupère la liste de type de pokemon
+  getPokemonTypeList(): string[] {
+    return [
+      "Plante",
+      "feu",
+      "Eau",
+      "Insecte",
+      "Normal",
+      "Electrik",
+      "Poison",
+      "Fée",
+      "Vol",
+      "Combat",
+      "Psy",
+    ];
+  }
 }
