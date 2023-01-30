67c348ba7de07110338952f90708da0a5f664fa5
brahim ouass <brahim.ouass@hotmail.fr>
2023-01-30 10:52:49 +0100

    page not found 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
 import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
   //décrire les routes les plus spécifiques en haut et les plus générales en bas.
   { path: "pokemons", component: ListPokemonComponent },
   { path: "pokemon/:id", component: DetailPokemonComponent },
   { path: "", redirectTo: "pokemons", pathMatch: "full" },
+  //page 404 à mettre toujours en dernier
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
