3da2451f4966d57d24d1550f1caec6c78e8507c0
brahim ouass <brahim.ouass@hotmail.fr>
2023-01-30 14:43:25 +0100

    Réorganisation par module

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
+import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
+import { BorderCardDirective } from "./border-card.directive";
+import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
+import { RouterModule, Routes } from "@angular/router";
 
-
+const pokemonRoutes: Routes = [
+  //décrire les routes les plus spécifiques en haut et les plus générales en bas.
+  { path: "pokemons", component: ListPokemonComponent },
+  { path: "pokemon/:id", component: DetailPokemonComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+  ],
+  imports: [CommonModule, RouterModule.forChild(pokemonRoutes)],
 })
-export class PokemonModule { }
+export class PokemonModule {}
