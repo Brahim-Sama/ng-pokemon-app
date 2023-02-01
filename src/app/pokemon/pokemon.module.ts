5cc22d84648dc38b6587fe2400b8dd86ca219162
brahim ouass <brahim.ouass@hotmail.fr>
2023-02-01 14:59:15 +0100

    Maj

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -8,6 +8,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { PokemonService } from "./pokemon.service";
 import { FormsModule } from "@angular/forms";
 import { PokemonFormComponent } from "./pokemon-form/pokemon-form.component";
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 
 const pokemonRoutes: Routes = [
   //décrire les routes les plus spécifiques en haut et les plus générales en bas.
@@ -22,6 +23,7 @@ const pokemonRoutes: Routes = [
     BorderCardDirective,
     PokemonTypeColorPipe,
     PokemonFormComponent,
+    EditPokemonComponent,
   ],
   imports: [CommonModule, FormsModule, RouterModule.forChild(pokemonRoutes)],
   providers: [PokemonService],
