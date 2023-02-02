27c42c36860443bc98f5c8bcb5ec0fa0a128207e
brahim ouass <brahim.ouass@hotmail.fr>
2023-02-02 15:09:43 +0100

    Requete HTTP

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+import { Observable } from "rxjs";
+import { POKEMONS } from "./pokemon/mock-pokemon-list";
+
+@Injectable({
+  providedIn: "root",
+})
+export class InMemoryDataService implements InMemoryDbService {
+  constructor() {}
+  // createDb(reqInfo?: RequestInfo | undefined): {} | Observable<{}> | Promise<{}> {
+  //   throw new Error("Method not implemented.");
+  // }
+
+  createDb() {
+    return { POKEMONS };
+  }
+}
--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,17 +1,35 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
+import { catchError, Observable, of, tap } from "rxjs";
 import { Pokemon } from "./pokemon";
 
 @Injectable()
 export class PokemonService {
+  constructor(private http: HttpClient) {}
+
   //récupère la liste de pokemon
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>("api/pokemons").pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, []))
+    );
   }
 
   // récupère le pokemon selon son ID
-  getPokemonById(pokemonId: number): Pokemon | undefined {
-    return POKEMONS.find((pokemon) => pokemon.id == pokemonId);
+  getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
+    return this.http.get<Pokemon>(`api/pokemon/${pokemonId}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, undefined))
+    );
+  }
+
+  private log(response: Pokemon[] | Pokemon | undefined) {
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
   }
 
   //récupère la liste de type de pokemon
